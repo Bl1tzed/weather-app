484b88f30b1a46ac969c8a73aeadaa72ca49fa3e
Bl1tzed <dns-2001@mail.ru>
2024-01-09 13:22:26 +0600

    fix: autofocus on LocationInput

--- src/LocationInput.jsx
+++ src/LocationInput.jsx
@@ -48,6 +48,7 @@ export default function LocationInput({ setLocation }) {
         onChange={handleOnChange}
         loadOptions={loadOptions}
         loadOptionsOnMenuOpen={false}
+        autoFocus
       />
     </div>
   );
